bc4454decac5650a0ff783b1762578a52bd1f09e
gussrivero <gustavo.rivero@mercadolibre.com>
2021-12-29 12:52:26 -0300

    First

---
+++ app/src/main/java/com/example/android_integrator/views/ActivitiesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_integrator.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_integrator.R
+
+class ActivitiesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activitis)
+    }
+}
---
+++ app/src/main/java/com/example/android_integrator/views/StartActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android_integrator.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_integrator.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+
+
+}
