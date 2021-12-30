106a833fd5f33d4401e72e9df795f11695f9a21e
mhmendezmeli <martinhoracio.mendez@mercadolibre.com>
2021-12-30 09:27:57 -0300

    Commit some navegation

--- app/src/main/java/com/example/android_integrator/views/StartActivity.kt
+++ app/src/main/java/com/example/android_integrator/views/StartActivity.kt
@@ -1,13 +1,33 @@
 package com.example.android_integrator.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.android_integrator.R
+import com.example.android_integrator.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var binding: ActivityStartBinding
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+                binding = ActivityStartBinding.inflate(layoutInflater)
+                setContentView(binding.root)
         setContentView(R.layout.activity_start)
+
+                setOnClickListeners()
+
+            }
+                 private fun setOnClickListeners() {
+                    binding.btnstart.setOnClickListener {
+                        Toast.makeText(this,"hola",Toast.LENGTH_LONG).show()
+                      //  val intent = Intent(this, ActivitiesActivity::class.java)
+                        //intent.putExtra("participants", binding.ETNumParticipants.inputType)
+                      //  startActivity(intent)
+
+                    }
+
     }
 
 
